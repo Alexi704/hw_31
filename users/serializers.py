defe597a660e9064757d04f1a6b0d5dac6b4d4c0
Tuman8 <i-a-l-e-x@yandex.ru>
2022-08-10 04:48:47 +0700

    поправлено хеширование паролей

--- users/serializers.py
+++ users/serializers.py
@@ -20,24 +20,50 @@ class UserSerializer(serializers.ModelSerializer):
         exclude = ['password']
 
 
+# class UserCreateSerializer(serializers.ModelSerializer):
+#     id = serializers.IntegerField(required=False)
+#     locations = serializers.SlugRelatedField(
+#         required=False,
+#         queryset=Location.objects.all(),
+#         many=True,
+#         slug_field='name',
+#     )
+#     def create(self, validated_data):
+#         user = super().create(validated_data)
+#         user.set_password(user.password)
+#         user.save()
+#         return user
+#     class Meta:
+#         model = User
+#         exclude = ['password']
+
 class UserCreateSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
     locations = serializers.SlugRelatedField(
         required=False,
         queryset=Location.objects.all(),
         many=True,
-        slug_field='name',
+        slug_field="name"
     )
 
+    def is_valid(self, raise_exception=False):
+        self._locations = self.initial_data.pop("locations", [])
+        return super().is_valid(raise_exception=raise_exception)
+
     def create(self, validated_data):
-        user = super().create(validated_data)
-        user.set_password(user.password)
+        user = User.objects.create(**validated_data)
+
+        user.set_password(validated_data["password"])
         user.save()
+
+        for locations in self._locations:
+            obj, _ = Location.objects.get_or_create(name=locations)
+            user.locations.add(obj)
+
         return user
 
     class Meta:
         model = User
-        exclude = ['password']
+        fields = '__all__'
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
